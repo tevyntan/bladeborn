353a444f624a182cada57f0730ada61e134ae8b3
tevyntan <150017244+tevyntan@users.noreply.github.com>
2025-05-29 08:29:30 +0800

    Enemy added
    added enemy
    added Global.gd script
    add player - enemy attack interaction

---
+++ Scripts/global.gd
@@ -0,0 +1,11 @@
+extends Node
+
+#Player
+var PlayerBody = CharacterBody2D
+var PlayerDmgZone: Area2D
+var PlayerDmgAmt: int
+var PlayerAlive: bool
+
+#Satyr
+var SatyrDmgZone: Area2D
+var SatyrDmgAmt: int

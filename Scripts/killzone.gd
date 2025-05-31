198c20fc99b0a77815fb3cd5e2caea5501deb385
isyraq <misyraq2003@gmail.com>
2025-05-31 21:03:44 +0400

    fixed bug where enemy falling off the map causes game to restart

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -3,8 +3,9 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	print("You died.")
-	timer.start()
+	if body.name == "Player":
+		print("You died.")
+		timer.start()
 	
 	
 	

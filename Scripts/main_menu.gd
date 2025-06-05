99326c04af03520f1d060366f69168cbc3fc8755
isyraq <misyraq2003@gmail.com>
2025-06-05 17:23:32 +0400

    added preliminary settings functionality

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,9 +1,12 @@
 extends Control
 
+@onready var main_menu_buttons: VBoxContainer = $MainMenuButtons
+@onready var settings_pop_up: Panel = $SettingsPopUp
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	main_menu_buttons.visible = true
+	settings_pop_up.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,8 +23,13 @@ func _on_load_pressed() -> void:
 
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	main_menu_buttons.visible = false;
+	settings_pop_up.visible = true;
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_settings_pressed() -> void:
+	_ready()

0f5fd1e0c4576d6b0d2d66860bb416bc2fc63084
isyraq <misyraq2003@gmail.com>
2025-06-19 17:10:08 +0800

    centralised powerups into singular node

--- Scripts/chest.gd
+++ Scripts/chest.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var powerups: Control = $"../CanvasLayer/Powerups"
-@onready var double_jump_screen: Control = $"../CanvasLayer/DoubleJumpScreen"
+@onready var double_jump_screen: Control = $"../CanvasLayer/PowerupScenes/DoubleJumpScreen"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:

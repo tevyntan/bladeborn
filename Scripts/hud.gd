e23cf6d366b52b11b1434193f7c256bd3411cfbf
tevyntan <150017244+tevyntan@users.noreply.github.com>
2025-06-28 17:39:31 +0800

    change back pause

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,18 +1,17 @@
 extends Control
 
+@onready var label: Label = $VBoxContainer/Heart/Label
 @onready var double_jump: Control = $DoubleJump
 
-var hearts_list : Array[TextureRect]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for child in $VBoxContainer/Hearts.get_children():
-		hearts_list.append(child)
-	
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	var player_health = Global.PlayerHeath
-	update_heart_display(player_health)
+func _process(delta: float) -> void:
+	label.text = str(Global.PlayerHeath)
+
 	if Global.PlayerFullMoon:
 		$VBoxContainer/MoonStyle/Full_logo.visible = true
 		$VBoxContainer/MoonStyle/Crescent_logo.visible = false
@@ -20,7 +19,3 @@ func _process(_delta: float) -> void:
 	else: 
 		$VBoxContainer/MoonStyle/Full_logo.visible = false
 		$VBoxContainer/MoonStyle/Crescent_logo.visible = true
-
-func update_heart_display(player_health):
-	for i in range(hearts_list.size()):
-		hearts_list[i].visible = (i * 20) < player_health
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -350.0
+const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var deal_dmg_zone: Area2D = $DealDmgZone
@@ -172,7 +172,6 @@ func take_damage(damage):
 
 func handle_death():
 	velocity.x = 0
-	velocity += get_gravity() * 1
 	if moon:
 		animated_sprite_2d.play("Full.Death")
 	else:
